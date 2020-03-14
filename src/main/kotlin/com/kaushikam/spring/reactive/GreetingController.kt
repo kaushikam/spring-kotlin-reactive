b94064b042b3f26a7894665e72d8891ed1ba301b
Kaushik Asokan <kaushikam@gmail.com>
2020-03-14 11:02:14 +0530

    Returning flow

--- src/main/kotlin/com/kaushikam/spring/reactive/GreetingController.kt
+++ src/main/kotlin/com/kaushikam/spring/reactive/GreetingController.kt
@@ -1,6 +1,9 @@
 package com.kaushikam.spring.reactive
 
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flow
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,8 +14,9 @@ import org.springframework.web.reactive.function.client.awaitExchange
 
 @RestController
 class GreetingController {
+    @FlowPreview
     @GetMapping("/hello")
-    suspend fun hello(): String {
+    suspend fun hello(): Flow<String> {
         return coroutineScope {
             val list = mutableListOf<Deferred<String>>()
             for (i in 1..100) {
@@ -22,8 +26,8 @@ class GreetingController {
                             .awaitExchange().awaitBody<String>()
                 })
             }
-            list.map { it.await() + "\n" }.joinToString(",")
-        }
+            list
+        }.map { it.await() }.asFlow()
     }
 
     private suspend fun client(): WebClient {
